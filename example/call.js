b66ea4c00744bb2e685a489862b3e06315b29034
Wanghaizhou <geekberry@qq.com>
2019-12-30 15:44:15 +0800

    FIX: doc

--- example/call.js
+++ example/call.js
@@ -16,33 +16,33 @@ async function main() {
     abi: require('./contract/abi.json'),
   });
 
-  // /**
-  //  * call contract function.
-  //  * `count` is a function name, see `solidity.sol`
-  //  */
-  // console.log(await contract.count()); // BigNumber { _hex: '0x0a' }, 10 in hex set by 'contract.constructor(10)'
-  //
-  // /**
-  //  * call contract function with params.
-  //  * (Executes a message call transaction, which is directly executed in the VM of the node, but never mined into the block chain.)
-  //  */
-  // console.log(await contract.inc(1)); // BigNumber { _hex: '0x0b' }, 11=10+1
-  //
-  // /**
-  //  * get a encode data
-  //  * (if you not await a called method, it will not send a rpc call)
-  //  */
-  // console.log(contract.inc(1).data); // 0x812600df0000000000000000000000000000000000000000000000000000000000000001
-  //
-  // /**
-  //  * as you see, above call operation not change the count!
-  //  */
-  // console.log(await contract.count()); // BigNumber { _hex: '0x0a' }
-  //
-  // /**
-  //  * `self` is a function which emit a `SelfEvent` event, but will not level log cause `cfx_call` operate.
-  //  */
-  // console.log(await contract.self()); // undefined
+  /**
+   * call contract function.
+   * `count` is a function name, see `solidity.sol`
+   */
+  console.log(await contract.count()); // BigNumber { _hex: '0x0a' }, 10 in hex set by 'contract.constructor(10)'
+
+  /**
+   * call contract function with params.
+   * (Executes a message call transaction, which is directly executed in the VM of the node, but never mined into the block chain.)
+   */
+  console.log(await contract.inc(1)); // BigNumber { _hex: '0x0b' }, 11=10+1
+
+  /**
+   * get a encode data
+   * (if you not await a called method, it will not send a rpc call)
+   */
+  console.log(contract.inc(1).data); // 0x812600df0000000000000000000000000000000000000000000000000000000000000001
+
+  /**
+   * as you see, above call operation not change the count!
+   */
+  console.log(await contract.count()); // BigNumber { _hex: '0x0a' }
+
+  /**
+   * `self` is a function which emit a `SelfEvent` event, but will not level log cause `cfx_call` operate.
+   */
+  console.log(await contract.self()); // undefined
 
   const iter = contract.SelfEvent().getLogs({ fromEpoch: 545896, toEpoch: 660500 });
   for await (const log of iter) {
