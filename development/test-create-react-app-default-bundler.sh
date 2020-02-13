eba58f10a5623987dbdecb0b9f7d2c8003d3a486
yqrashawn <namy.19@gmail.com>
2020-02-13 11:02:49 +0800

    Fix: sed script in ci test

--- development/test-create-react-app-default-bundler.sh
+++ development/test-create-react-app-default-bundler.sh
@@ -10,5 +10,6 @@ create-react-app test-sdk-frontend-import-bundler
 cd test-sdk-frontend-import-bundler
 
 # prepend import { Conflux } from '../../'; to cra's default App.js
-sed -i.old "1s;^;import { Conflux } from '../../';" src/App.js
+sed -i.old -e '1 i\
+import { Conflux } from "../../"\;' src/App.js
 yarn build
