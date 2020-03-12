2c817b93caa6809789cc19a400ab68d170f871e8
Wanghaizhou <geekberry@qq.com>
2020-03-12 11:54:16 +0800

    UPDATE: unit support float

--- test/util/unit.test.js
+++ test/util/unit.test.js
@@ -2,43 +2,64 @@ const JSBI = require('jsbi');
 const { util: { unit } } = require('../../src');
 
 test('unit', () => {
-  expect(unit('cfx', 'cfx')(1)).toEqual(JSBI.BigInt(1));
-  expect(unit('cfx', 'gdrip')(1)).toEqual(JSBI.BigInt(1e9));
-  expect(unit('cfx', 'drip')(1)).toEqual(JSBI.BigInt(1e18));
+  expect(unit('CFX', 'CFX')(1)).toEqual(JSBI.BigInt(1));
+  expect(unit('CFX', 'GDrip')(1)).toEqual(JSBI.BigInt(1e9));
+  expect(unit('CFX', 'Drip')(1)).toEqual(JSBI.BigInt(1e18));
 
-  expect(unit('gdrip', 'cfx')(1e9)).toEqual(JSBI.BigInt(1));
-  expect(unit('gdrip', 'gdrip')(1)).toEqual(JSBI.BigInt(1));
-  expect(unit('gdrip', 'drip')(1)).toEqual(JSBI.BigInt(1e9));
+  expect(unit('GDrip', 'CFX')(1e9)).toEqual(JSBI.BigInt(1));
+  expect(unit('GDrip', 'GDrip')(1)).toEqual(JSBI.BigInt(1));
+  expect(unit('GDrip', 'Drip')(1)).toEqual(JSBI.BigInt(1e9));
 
-  expect(unit('drip', 'cfx')(1e18)).toEqual(JSBI.BigInt(1));
-  expect(unit('drip', 'gdrip')(1e9)).toEqual(JSBI.BigInt(1));
-  expect(unit('drip', 'drip')(1)).toEqual(JSBI.BigInt(1));
+  expect(unit('Drip', 'CFX')(1e18)).toEqual(JSBI.BigInt(1));
+  expect(unit('Drip', 'GDrip')(1e9)).toEqual(JSBI.BigInt(1));
+  expect(unit('Drip', 'Drip')(1)).toEqual(JSBI.BigInt(1));
 
-  expect(() => unit('CFX', 'drip')).toThrow('"from" must in ["cfx","gdrip","drip"], got "CFX"');
-  expect(() => unit('cfx', 'Drip')).toThrow('"to" must in ["cfx","gdrip","drip"], got "Drip"');
+  expect(() => unit('CFX', 'drip')).toThrow('"to" must in ["CFX","GDrip","Drip"], got "drip"');
+  expect(() => unit('cfx', 'Drip')).toThrow('"from" must in ["CFX","GDrip","Drip"], got "cfx"');
 });
 
 test('fromCFXToGDrip', () => {
+  expect(unit.fromCFXToGDrip(-0.1)).toEqual(JSBI.BigInt(-100000000));
   expect(unit.fromCFXToGDrip(123)).toEqual(JSBI.BigInt(123000000000));
-});
+  expect(unit.fromCFXToGDrip('123')).toEqual(JSBI.BigInt(123000000000));
 
-test('fromGDripToCFX', () => {
-  expect(unit.fromGDripToCFX(123456789012)).toEqual(JSBI.BigInt(123));
-  expect(unit.fromGDripToCFX(123999999999)).toEqual(JSBI.BigInt(123));
-});
-
-test('fromCFXToDrip', () => {
-  expect(unit.fromCFXToDrip(123)).toEqual(JSBI.BigInt(123000000000000000000));
-});
-
-test('fromDripToCFX', () => {
-  expect(unit.fromDripToCFX(123456789012345678901)).toEqual(JSBI.BigInt(123));
+  expect(unit.fromCFXToGDrip(0.123456789)).toEqual(JSBI.BigInt('123456789'));
+  expect(() => unit.fromCFXToGDrip('0.1234567891')).toThrow('Cannot convert 123456789.1');
 });
 
 test('fromGDripToDrip', () => {
+  expect(unit.fromGDripToDrip(-0.1)).toEqual(JSBI.BigInt(-100000000));
   expect(unit.fromGDripToDrip(123)).toEqual(JSBI.BigInt(123000000000));
+  expect(unit.fromGDripToDrip('123')).toEqual(JSBI.BigInt(123000000000));
+
+  expect(unit.fromGDripToDrip(0.123456789)).toEqual(JSBI.BigInt('123456789'));
+  expect(() => unit.fromGDripToDrip('0.1234567891')).toThrow('Cannot convert 123456789.1');
 });
 
 test('fromDripToGDrip', () => {
-  expect(unit.fromDripToGDrip(123456789012)).toEqual(JSBI.BigInt(123));
+  expect(unit.fromDripToGDrip(1000000000)).toEqual(JSBI.BigInt(1));
+  expect(unit.fromDripToGDrip(-1000000000)).toEqual(JSBI.BigInt(-1));
+  expect(() => unit.fromDripToGDrip(1100000000)).toThrow('Cannot convert 1.1');
+});
+
+test('fromGDripToCFX', () => {
+  expect(unit.fromGDripToCFX(1000000000)).toEqual(JSBI.BigInt(1));
+  expect(unit.fromGDripToCFX(-1000000000)).toEqual(JSBI.BigInt(-1));
+  expect(() => unit.fromGDripToCFX(1100000000)).toThrow('Cannot convert 1.1');
+});
+
+test('fromCFXToDrip', () => {
+  expect(unit.fromCFXToDrip(-0.1)).toEqual(JSBI.BigInt(-100000000000000000));
+  expect(unit.fromCFXToDrip(123)).toEqual(JSBI.BigInt(123000000000000000000));
+  expect(unit.fromCFXToDrip('123')).toEqual(JSBI.BigInt(123000000000000000000));
+
+  expect(unit.fromCFXToDrip('0.123456789123456789')).toEqual(JSBI.BigInt('123456789123456789'));
+  expect(() => unit.fromCFXToDrip('0.1234567891234567891')).toThrow('Cannot convert 123456789123456789.1');
+});
+
+test('fromDripToCFX', () => {
+  expect(unit.fromDripToCFX('1000000000000000000')).toEqual(JSBI.BigInt(1));
+  expect(unit.fromDripToCFX('-1000000000000000000')).toEqual(JSBI.BigInt(-1));
+
+  expect(() => unit.fromDripToCFX('1100000000000000000')).toThrow('Cannot convert 1.1');
 });
