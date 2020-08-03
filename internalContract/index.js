d9f373e219dde5f999443decc8f1156a0c52394b
Wanghaizhou <geekberry@qq.com>
2020-08-03 16:11:26 +0800

    ADD: InternalContract

---
+++ internalContract/index.js
@@ -0,0 +1,9 @@
+const AdminControl = require('./AdminControl.json');
+const SponsorWhitelistControl = require('./SponsorWhitelistControl.json');
+const Staking = require('./Staking.json');
+
+module.exports = {
+  AdminControl,
+  SponsorWhitelistControl,
+  Staking,
+};
