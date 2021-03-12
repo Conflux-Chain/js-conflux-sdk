cbb20d45508aeb318276c3a9579bb79dcc2a0401
PanaW <wangpan@conflux-chain.org>
2021-03-12 22:58:13 +0800

    fix lint error

--- src/util/providerWrapper.js
+++ src/util/providerWrapper.js
@@ -6,19 +6,20 @@ const format = require('./format');
 function parseSignature(sig) {
   return {
     r: sig.slice(0, 66),
-    s: '0x' + sig.slice(66, 66 + 64),
-    v: Number('0x' + sig.slice(66 + 64, 66 + 66)) - 27,
+    s: `0x${sig.slice(66, 66 + 64)}`,
+    v: Number(`0x${sig.slice(66 + 64, 66 + 66)}`) - 27,
   };
 }
 
 async function signWithMetaMask(txInfo) {
-  let tx = new Transaction(txInfo);
-  let unsignedHash = format.hex(sign.keccak256(tx.encode(false)));
-  let signature = await ethereum.request({
+  const tx = new Transaction(txInfo);
+  const unsignedHash = format.hex(sign.keccak256(tx.encode(false)));
+  /* eslint-disable */
+  const signature = await ethereum.request({
     method: 'eth_sign',
-    params: [ethereum.selectedAddress, unsignedHash]
+    params: [ethereum.selectedAddress, unsignedHash],
   });
-  let sigInfo = parseSignature(signature);
+  const sigInfo = parseSignature(signature);
   tx.r = sigInfo.r;
   tx.s = sigInfo.s;
   tx.v = sigInfo.v;
@@ -27,16 +28,16 @@ async function signWithMetaMask(txInfo) {
 
 async function useEthereumPrepareTx(txInfo, callRPC) {
   if (!txInfo.chainId) {
-    txInfo.chainId = await callRPC({method: 'eth_chainId'});
+    txInfo.chainId = await callRPC({ method: 'eth_chainId' });
   }
   if (!txInfo.gasPrice) {
     txInfo.gasPrice = MIN_GAS_PRICE;
   }
   if (!txInfo.nonce) {
-    txInfo.nonce = await callRPC({method: 'eth_getTransactionCount', params: [txInfo.from]});
+    txInfo.nonce = await callRPC({ method: 'eth_getTransactionCount', params: [txInfo.from] });
   }
   if (!txInfo.epochHeight) {
-    txInfo.epochHeight = await callRPC({method: 'eth_blockNumber', params: []});
+    txInfo.epochHeight = await callRPC({ method: 'eth_blockNumber', params: [] });
   }
 }
 
@@ -44,20 +45,20 @@ function wrapEthereum(provider) {
   if (typeof ethereum === 'undefined') {
     throw new Error('MetaMask is not installed!');
   }
-  let originRequest = provider.request;
+  const originRequest = provider.request;
 
   async function request(payload) {
-    let {method, params} = payload;
-    if (method != 'eth_sendTransaction') {
-      return await originRequest(payload);
+    const { method, params } = payload;
+    if (method !== 'eth_sendTransaction') {
+      return originRequest(payload);
     }
-    let txInfo = params[0];
+    const txInfo = params[0];
     if (!txInfo.gas || !txInfo.storageLimit) {
       throw new Error("'gas' and 'storageLimit' field is needed");
     }
     await useEthereumPrepareTx(txInfo, originRequest);
-    let rawTx = await signWithMetaMask(txInfo);
-    return await originRequest({
+    const rawTx = await signWithMetaMask(txInfo);
+    return originRequest({
       method: 'eth_sendRawTransaction',
       params: [rawTx],
     });
@@ -68,12 +69,12 @@ function wrapEthereum(provider) {
 
 async function useConfluxPrepareTx(txInfo, callRPC) {
   if (!txInfo.chainId) {
-    let {chainId} = await callRPC('cfx_getStatus');
+    const { chainId } = await callRPC('cfx_getStatus');
     txInfo.chainId = chainId;
   }
   if (!txInfo.gas || !txInfo.storageLimit) {
     txInfo = format.callTxAdvance(txInfo.chainId)(txInfo);
-    let { gasLimit, storageCollateralized } = await callRPC('cfx_estimateGasAndCollateral', txInfo);
+    const { gasLimit, storageCollateralized } = await callRPC('cfx_estimateGasAndCollateral', txInfo);
     if (!txInfo.gas) {
       txInfo.gas = gasLimit;
     }
@@ -97,16 +98,16 @@ function wrapConflux(provider) {
   if (typeof ethereum === 'undefined') {
     throw new Error('MetaMask is not installed!');
   }
-  let originRequest = provider.call.bind(provider);
+  const originRequest = provider.call.bind(provider);
 
   async function request(method, ...params) {
     if (method !== 'cfx_sendTransaction') {
-      return await originRequest(method, params);
+      return originRequest(method, params);
     }
     let txInfo = params[0];
     txInfo = await useConfluxPrepareTx(txInfo, originRequest);
-    let rawTx = await signWithMetaMask(txInfo);
-    return await originRequest('cfx_sendRawTransaction', rawTx);
+    const rawTx = await signWithMetaMask(txInfo);
+    return originRequest('cfx_sendRawTransaction', rawTx);
   }
 
   provider.call = request.bind(provider);
