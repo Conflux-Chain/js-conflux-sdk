86014f19f5aae1b2aa1a113f5e3bbc4aef5941f7
PanaW <pan.wang@confluxnetwork.org>
2021-11-09 23:19:56 +0800

    update internal contract abi

--- src/contract/internal/index.js
+++ src/contract/internal/index.js
@@ -1,15 +1,13 @@
 const AdminControl = require('./AdminControl.json');
 const SponsorWhitelistControl = require('./SponsorWhitelistControl.json');
 const Staking = require('./Staking.json');
-const ReentrancyConfig = require('./ReentrancyConfig.json');
-const EpochContext = require('./EpochContext.json');
+const ConfluxContext = require('./ConfluxContext.json');
 const PoSRegister = require('./PoSRegister.json');
 
 module.exports = {
   AdminControl,
   SponsorWhitelistControl,
   Staking,
-  ReentrancyConfig,
-  EpochContext,
+  ConfluxContext,
   PoSRegister,
 };
