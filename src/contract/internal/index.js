b362a705d3ebac2c938d0eaaeecc35343acaba85
PanaW <pana.wang@outlook.com>
2022-01-11 14:36:52 +0800

    use readable abi for internal contract

--- src/contract/internal/index.js
+++ src/contract/internal/index.js
@@ -1,9 +1,82 @@
-const AdminControl = require('./AdminControl.json');
-const SponsorWhitelistControl = require('./SponsorWhitelistControl.json');
-const Staking = require('./Staking.json');
-const ConfluxContext = require('./ConfluxContext.json');
-const PoSRegister = require('./PoSRegister.json');
-const CrossSpaceCall = require('./CrossSpaceCall.json');
+const AdminControl = {
+  abi: [
+    'function getAdmin(address contractAddr) public view returns (address)',
+    'function setAdmin(address contractAddr, address newAdmin)',
+    'function destroy(address contractAddr)',
+  ],
+  address: '0x0888000000000000000000000000000000000000',
+};
+
+const SponsorWhitelistControl = {
+  abi: [
+    'function getSponsorForGas(address contractAddr) public view returns (address)',
+    'function getSponsoredBalanceForGas(address contractAddr) public view returns (uint256)',
+    'function getSponsoredGasFeeUpperBound(address contractAddr) public view returns (uint256)',
+    'function getSponsorForCollateral(address contractAddr) public view returns (address)',
+    'function getSponsoredBalanceForCollateral(address contractAddr) public view returns (uint256)',
+    'function isWhitelisted(address contractAddr, address user) public view returns (bool)',
+    'function isAllWhitelisted(address contractAddr) public view returns (bool)',
+    'function addPrivilegeByAdmin(address contractAddr, address[] memory addresses)',
+    'function removePrivilegeByAdmin(address contractAddr, address[] memory addresses)',
+    'function setSponsorForGas(address contractAddr, uint upperBound)',
+    'function setSponsorForCollateral(address contractAddr)',
+    // 'function addPrivilege(address[] memory)',
+    // 'function removePrivilege(address[] memory)',
+  ],
+  address: '0x0888000000000000000000000000000000000001',
+};
+
+const Staking = {
+  abi: [
+    'function getStakingBalance(address user) public view returns (uint256)',
+    'function getLockedStakingBalance(address user, uint256 blockNumber) public view returns (uint256)',
+    'function getVotePower(address user, uint256 blockNumber) public view returns (uint256)',
+    'function deposit(uint256 amount)',
+    'function withdraw(uint256 amount)',
+    'function voteLock(uint256 amount, uint256 unlockBlockNumber)',
+  ],
+  address: '0x0888000000000000000000000000000000000002',
+};
+
+const ConfluxContext = {
+  abi: [
+    'function epochNumber() public view returns (uint256)',
+    'function posHeight() public view returns (uint256)',
+    'function finalizedEpochNumber() public view returns (uint256)',
+  ],
+  address: '0x0888000000000000000000000000000000000004',
+};
+
+const PoSRegister = {
+  abi: [
+    'function register(bytes32 indentifier, uint64 votePower, bytes calldata blsPubKey, bytes calldata vrfPubKey, bytes[2] calldata blsPubKeyProof)',
+    'function increaseStake(uint64 votePower)',
+    'function retire(uint64 votePower)',
+    'function getVotes(bytes32 identifier) external view returns (uint256, uint256)',
+    'function identifierToAddress(bytes32 identifier) external view returns (address)',
+    'function addressToIdentifier(address addr) external view returns (bytes32)',
+    'event Register(bytes32 indexed identifier, bytes blsPubKey, bytes vrfPubKey)',
+    'event IncreaseStake(bytes32 indexed identifier, uint64 votePower)',
+  ],
+  address: '0x0888000000000000000000000000000000000005',
+};
+
+const CrossSpaceCall = {
+  abi: [
+    'event Call(bytes20 indexed sender, bytes20 indexed receiver, uint256 value, uint256 nonce, bytes data)',
+    'event Create(bytes20 indexed sender, bytes20 indexed contract_address, uint256 value, uint256 nonce, bytes init)',
+    'event Withdraw(bytes20 indexed sender, address indexed receiver, uint256 value)',
+    'function createEVM(bytes calldata init) external payable returns (bytes20)',
+    'function create2EVM(bytes calldata init, bytes32 salt) external payable returns (bytes20)',
+    'function transferEVM(bytes20 to) external payable returns (bytes memory output)',
+    'function callEVM(bytes20 to, bytes calldata data) external payable returns (bytes memory output)',
+    'function staticCallEVM(bytes20 to, bytes calldata data) external view returns (bytes memory output)',
+    'function withdrawFromMapped(uint256 value)',
+    'function mappedBalance(address addr) external view returns (uint256)',
+    'function mappedNonce(address addr) external view returns (uint256)',
+  ],
+  address: '0x0888000000000000000000000000000000000006',
+};
 
 module.exports = {
   AdminControl,
--- src/contract/standard/index.js
+++ src/contract/standard/index.js
@@ -1,4 +1,18 @@
-const CRC20_ABI = require('./crc20.json').abi;
+const CRC20_ABI = [
+  'function name() view returns (string)',
+  'function symbol() view returns (string)',
+  'function decimals() view returns (uint8)',
+  'function totalSupply() view returns (uint256)',
+  'function balanceOf(address) view returns (uint256)',
+  'function transfer(address to, uint256 amount)',
+  'function allowance(address owner, address spender) view returns (uint256)',
+  'function approve(address spender, uint256 amount) returns (bool)',
+  'function transferFrom(address sender, address recipient, uint256 amount) returns (bool)',
+  'function increaseAllowance(address spender, uint256 amount) returns (bool)',
+  'function decreaseAllowance(address spender, uint256 amount) returns (bool)',
+  'event Transfer(address indexed from, address indexed to, uint256 amount)',
+  'event Approval(address indexed owner, address indexed spender, uint256 amount)',
+];
 
 module.exports = {
   CRC20_ABI,
