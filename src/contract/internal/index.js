a53efc8c0b457806f9be67001d22c418748842a9
PanaW <pana.wang@outlook.com>
2022-01-20 16:50:27 +0800

    remove create2EVM

--- src/contract/internal/index.js
+++ src/contract/internal/index.js
@@ -68,7 +68,6 @@ const CrossSpaceCall = {
     'event Withdraw(bytes20 indexed sender, address indexed receiver, uint256 value)',
     'event Outcome(uint256 nonce, uint256 gas_left, bool success)',
     'function createEVM(bytes calldata init) external payable returns (bytes20)',
-    'function create2EVM(bytes calldata init, bytes32 salt) external payable returns (bytes20)',
     'function transferEVM(bytes20 to) external payable returns (bytes memory output)',
     'function callEVM(bytes20 to, bytes calldata data) external payable returns (bytes memory output)',
     'function staticCallEVM(bytes20 to, bytes calldata data) external view returns (bytes memory output)',
