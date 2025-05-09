fba519258cfff66050daaf27f833f600cc0dbe18
Pana <pana.wang@outlook.com>
2025-05-09 17:55:44 +0800

    add rpcMethod and rpcParams to parser error when calling rpc method

--- src/rpc/index.js
+++ src/rpc/index.js
@@ -21,23 +21,35 @@ class RPCMethodFactory {
 
   createRPCMethod({ method, requestFormatters = [], responseFormatter = format.any, beforeHook }) {
     async function rpcMethod(...args) {
-      if (beforeHook) {
-        beforeHook(...args);
+      try {
+        if (beforeHook) {
+          beforeHook(...args);
+        }
+        const params = Array.from(args).map((arg, i) => (requestFormatters[i] ? requestFormatters[i](arg) : arg));
+        const result = await this.conflux.request({ method, params });
+        return responseFormatter(result);
+      } catch(error) {
+        error.rpcMethod = method;
+        error.rpcParams = args;
+        throw error;
       }
-      const params = Array.from(args).map((arg, i) => (requestFormatters[i] ? requestFormatters[i](arg) : arg));
-      const result = await this.conflux.request({ method, params });
-      return responseFormatter(result);
     }
 
     rpcMethod.request = function (...args) {
-      const params = Array.from(args).map((arg, i) => (requestFormatters[i] ? requestFormatters[i](arg) : arg));
-      return {
-        request: {
-          method,
-          params,
-        },
-        decoder: responseFormatter,
-      };
+      try {
+        const params = Array.from(args).map((arg, i) => (requestFormatters[i] ? requestFormatters[i](arg) : arg));
+        return {
+          request: {
+            method,
+            params,
+          },
+          decoder: responseFormatter,
+        };
+      } catch (error) {
+        error.rpcMethod = method;
+        error.rpcParams = args;
+        throw error;
+      }
     };
 
     return rpcMethod;
