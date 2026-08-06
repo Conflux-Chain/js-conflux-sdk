85722859d2d3780c0afa74d00ba75e0fa1957e21
Pana <pana.wang@outlook.com>
2026-08-06 11:34:28 +0800

    rpc error message compatible handle (#291)
    * rpc error message compatible deal
    
    * bump version
    
    * fix: preserve RPC parameter error details
    
    * remove quote from error detail
    
    add code check
    
    * update error detail sanitize logic to only remove outer double quotes

--- src/provider/RPCError.js
+++ src/provider/RPCError.js
@@ -1,6 +1,8 @@
 const { isHexString } = require('../util');
 const format = require('../util/format');
 
+const INVALID_PARAMS = 'Invalid params:';
+const INVALID_PARAMETERS = 'Invalid parameters:';
 class RPCError extends Error {
   constructor(object, payload = {}) {
     supplementErrorInfo(object, payload);
@@ -8,9 +10,33 @@ class RPCError extends Error {
     Object.assign(this, object);
     Object.assign(this, payload);
   }
+
+  // return the parameter error detail, return null if it is not an Invalid params error
+  paramsErrorDetail() {
+    if (this.code !== -32602) return null;
+    if (this.message === 'Invalid params' && typeof this.data === 'string' && this.data) {
+      return sanitizeParamsErrorDetail(this.data);
+    }
+    if (this.message.startsWith(INVALID_PARAMS)) {
+      return sanitizeParamsErrorDetail(this.message.substring(INVALID_PARAMS.length).trim());
+    }
+    if (this.message.startsWith(INVALID_PARAMETERS)) {
+      return sanitizeParamsErrorDetail(this.message.substring(INVALID_PARAMETERS.length).trim());
+    }
+    return null;
+  }
 }
 
 module.exports = RPCError;
+
+// Remove extra outer double quotes
+function sanitizeParamsErrorDetail(detail) {
+  if (detail[0] === '"' && detail[detail.length - 1] === '"') {
+    return JSON.parse(detail);
+  } else {
+    return detail;
+  }
+}
 
 function supplementErrorInfo(object, payload) {
   // If use base32 address with full node before v1.1.1, will encounter this error
